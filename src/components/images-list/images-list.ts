db417510e10b3ead81e5af0e81a61a1e541ec56e
VityaRa <blackops2in@mail.ru>
2021-06-09 17:27:23 +0300

    feat: add remove and add images funcs

--- src/components/images-list/images-list.ts
+++ src/components/images-list/images-list.ts
@@ -1,14 +1,38 @@
-import { BaseComponent } from "../base-component";
+import { BaseComponent } from '../base-component';
+import './images-list.scss';
 
 export class ImagesList extends BaseComponent {
+
   constructor() {
-    super('div', ['add-field__inner']);
+    super('div', ['galary__inner']);
+
+    document.addEventListener('AddImage', ((event: CustomEvent) => {
+      this.createImage(event.detail.url)
+    }) as EventListener);
+
+    document.addEventListener('click', (e) => {
+      this.removeImage(e)
+    });
   }
 
-  addImage(): void{
-
+  createImage(imageURL: string): void {
+    if (!imageURL) {
+      alert('Введите URL картинки')
+      return
+    }
+    fetch(imageURL)
+      .then(ans => ans.ok ? ans : Promise.reject(ans))
+      .then(ans => {
+        const img = new BaseComponent('img', ['galary__inner__image'])
+        img.element.setAttribute('src', imageURL);
+        this.element.appendChild(img.element);
+      })
+      .catch(() => {
+        alert('Error')
+      })
   }
-  removeImage(): void{
 
+  removeImage(sender: MouseEvent): void {
+    if ((<HTMLElement>sender.target).tagName === 'IMG') (<HTMLElement>sender.target).remove()
   }
 }
