420c33bdddc6a87d803395686768586c48285bcc
VityaRa <blackops2in@mail.ru>
2021-06-10 14:46:10 +0300

    feat: add styles for style.scss

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { App } from './components/app/app';
 import './style.scss';
 
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.querySelector('body');
   if (!rootElement) {
     alert('Error...');
     return;
   }
   rootElement.appendChild(new App().element);
-};
+})
